5228df008b9f35408b6dbbf8064993e2764bbea5
netng <net.nandang@gmail.com>
2022-05-31 04:39:38 +0000

    updating deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
-append :linked_dirs, "public", "vendor", "storage"
+append :linked_dirs, "vendor", "storage/app", "storage/framework/cache", "storage/framework/sessions", "storage/framework/views", "storage/logs"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
